8ae835d0c78f492cc9f49f9f2247ae534572c16c
lcti-caveira <136091050+lcti-caveira@users.noreply.github.com>
2023-06-09 21:54:21 +0200

    Testing message

--- source/responses.py
+++ source/responses.py
@@ -2,7 +2,7 @@ def handle_response(message) -> str:
     print(f'Handling {message}')
     p_message = message.lower()
     if p_message == 'hello':
-        return "What's up! Dude"
+        return "What's up!"
     if p_message == '!help':
         return "Just say hello"
     return "I can't understand that..."
